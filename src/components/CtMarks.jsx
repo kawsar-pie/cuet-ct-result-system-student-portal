393dfa136ea4ce4a622707aa60db3f8ad629aebf
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-01-27 20:01:13 +0600

    ct marks added

--- src/components/CtMarks.jsx
+++ src/components/CtMarks.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
-
-const CtMarks = () => {
-    return (
-        <div className='mt-4'>
-            <div className="overflow-x-hidden">
-                <table className="table">
-                    <thead className='text-md text-white text-center'>
-                        <tr>
-                            <th>CT-01</th>
-                            <th>CT-02</th>
-                            <th>CT-03</th>
-                            <th>CT-04</th>
-                            <th>Best Three</th>
-                            <th>Atten.</th>
-                        </tr>
-                    </thead>
-                    <tbody className='text-xl text-white text-center'>
-                        <tr>
-                            <th>20</th>
-                            <th>10</th>
-                            <th>15</th>
-                            <th>15</th>
-                            <th>50</th>
-                            <th><h4 className='flex flex-row items-center gap-1'><span>25</span> <sub className='text-xs'>({Math.ceil(25*100/30)}%)</sub></h4></th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+// eslint-disable-next-line react/prop-types
+const CtMarks = ({ classTests = [], total = 0, isLoading = false }) => {
+  return (
+    <div className="mt-4">
+      <div className="overflow-x-hidden">
+        {classTests.length > 0 ? (
+          <table className="table">
+            <thead className="text-md text-white text-center">
+              <tr>
+                {classTests.map((ct, idx) => (
+                  <th key={idx}>{`CT-${idx + 1}`}</th>
+                ))}
+                {/* <th>CT-02</th>
+              <th>CT-03</th>
+              <th>CT-04</th> */}
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody className="text-xl text-white text-center">
+              <tr>
+                {classTests.map((ct, idx) => (
+                  <th key={idx}>{ct.marks}</th>
+                ))}
+                <th>{total}</th>
+                {/* <th>
+                <h4 className="flex flex-row items-center gap-1">
+                  <span>25</span>{" "}
+                  <sub className="text-xs">({Math.ceil((25 * 100) / 30)}%)</sub>
+                </h4>
+              </th> */}
+              </tr>
+            </tbody>
+          </table>
+        ) : isLoading ? (
+          <h3 className="text-center">Loading...</h3>
+        ) : (
+          <h3 className="text-center">There is no Class Test results</h3>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CtMarks;
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -6,6 +6,9 @@ import CtMarks from "../components/CtMarks";
 const Courses = () => {
   const { semesterId } = useParams();
   const [courses, setCourses] = useState([]);
+  const [classTests, setClassTests] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState(0);
   useEffect(() => {
     const fetchCourse = async () => {
       const result = await fetchData(`/semesters/courses/${semesterId}`);
@@ -14,7 +17,21 @@ const Courses = () => {
       }
     };
     fetchCourse();
-  }, []);
+  }, [semesterId]);
+  const showResult = async (courseCode) => {
+    setClassTests([]);
+    setTotal(0);
+    setLoading(true);
+    const result = await fetchData(`/ct/results/${semesterId}/${courseCode}`);
+    if (result.success) {
+      const { results, total } = result.data;
+      setClassTests(results);
+      setTotal(total);
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  };
   return (
     <div className="">
       <div
@@ -27,7 +44,7 @@ const Courses = () => {
       >
         <div className="hero-overlay bg-opacity-70"></div>
         <div className="grid grid-cols-3 gap-16 py-[9%] px-[5%]">
-          {courses.map(({ course, teachers }) => (
+          {courses.map(({ course }) => (
             <>
               <div
                 data-aos="fade-down"
@@ -57,9 +74,10 @@ const Courses = () => {
                   <div className="card-actions justify-center">
                     <button
                       className="btn glass bg-success text-white"
-                      onClick={() =>
-                        document.getElementById("my_modal_5").showModal()
-                      }
+                      onClick={() => {
+                        showResult(course.courseCode);
+                        document.getElementById("my_modal_5").showModal();
+                      }}
                     >
                       Marks
                     </button>
@@ -97,10 +115,12 @@ const Courses = () => {
       </button> */}
       <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box bg-transparent glass text-white w-11/12 max-w-5xl">
-          <h3 className="font-bold text-lg text-center">
-            CT and Attendance Marks (CSE-423)
-          </h3>
-          <CtMarks></CtMarks>
+          <h3 className="font-bold text-lg text-center">Class Tests Result</h3>
+          <CtMarks
+            classTests={classTests}
+            total={total}
+            isLoading={loading}
+          ></CtMarks>
           <div className="modal-action">
             <form method="dialog">
               <button className="btn btn-success glass text-white">
